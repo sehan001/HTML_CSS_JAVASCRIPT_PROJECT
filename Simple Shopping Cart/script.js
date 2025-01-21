88fdb2ae0150d9d7c02fe89f46f1e4110e5ac889
sehan001 <nmsehan.001@gmail.com>
2025-01-22 01:06:43 +0530

    list functionality added

--- Simple Shopping Cart/script.js
+++ Simple Shopping Cart/script.js
@@ -1,1 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelectorAll(".add-input");
+  const addBtn = document.querySelector(".list-btn");
+
+  addBtn.addEventListener("click", () => {
+    const task = input[0].value.trim();
+    const price = input[1].value.trim();
+
+    // if either the task or price is empty, do not add anything to the list
+    // the return statement is used to exit the function early if either of the conditions is true
+    if (!task && !price) return;
+    const taskList = document.querySelector(".products ol");
+    const newTask = document.createElement("li");
+    newTask.classList.add("list-item");
+    newTask.textContent = `${task}     :     Rs. ${price}`;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", () => {
+      newTask.remove();
+    });
+    newTask.append(deleteBtn);
+    taskList.append(newTask);
+  });
+});
